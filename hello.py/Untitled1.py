ff8e31e59ae2fb2db2118b07faa318aa0badc4eb
Swathi Munikoti <swathi_ms@live.in>
2023-10-25 16:55:15 +0000

    Added 2 files on basic Python

---
+++ hello.py/Untitled1.py
@@ -0,0 +1,45 @@
+"""
+Your module description
+"""
+
+print("Hello, World")   
+print("Python has three numeric types: int, float, and complex")
+
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+name = input("What is your name? ")
+print(name)
+
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
+
